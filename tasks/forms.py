e4e0444f1cfc9eca491d467a708f26dacc6096f8
artklk12 <artem-kulik@list.ru>
2022-07-17 13:27:36 +0300

    fix bugs

--- tasks/forms.py
+++ tasks/forms.py
@@ -12,12 +12,6 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
-        widgets = {
-            'username': forms.TextInput(attrs={'class': 'form-control'}),
-            'email': forms.EmailInput(attrs={'class': 'form-control'}),
-            'password1': forms.PasswordInput(attrs={'class': 'form-control'}),
-            'password2': forms.PasswordInput(attrs={'class': 'form-control'}),
-        }
 
 class UserLoginForm(AuthenticationForm):
     username = forms.CharField(label='Username', widget=forms.TextInput(attrs={'class': 'form-control no-border'}))
