10ee14df7bcb8cd78274e2570344eb8fd2d36eb4
artklk12 <67318020+artklk12@users.noreply.github.com>
2022-09-05 18:05:56 +0300

    Update urls.py

--- wunderapi/urls.py
+++ wunderapi/urls.py
@@ -5,7 +5,7 @@ from rest_framework.routers import DefaultRouter
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
 router.register(r'tasks', views.TaskViewSet, basename='tasks')
-router.register(r'users', views.UserViewSet, basename='users')
+router.register(r'users', views.UserViewSet, basename='user')
 
 # The API URLs are now determined automatically by the router.
 urlpatterns = [
